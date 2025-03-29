546330c68cda7b6fbb6658105b666b73d68061c5
BoxBoxJason <contact@boxboxjason.dev>
2025-03-30 00:15:28 +0100

    build: concurrency upgrade Fix the improper initial logic Add concurrency Add verbosity

--- mirroring/get.go
+++ mirroring/get.go
@@ -2,72 +2,193 @@ package mirroring
 
 import (
 	"fmt"
-	"path"
+	"log"
+	"path/filepath"
 	"strings"
+	"sync"
 
 	"github.com/boxboxjason/gitlab-sync/utils"
+	gitlab "gitlab.com/gitlab-org/api/client-go"
 )
 
-func (g *GitlabInstance) fetchProjects(filters *utils.MirrorMapping) error {
+func (g *GitlabInstance) fetchProjects(projectFilters *map[string]bool, groupFilters *map[string]bool, mirrorMapping *utils.MirrorMapping, isSource bool) error {
+	sourceString := "source"
+	if !isSource {
+		sourceString = "destination"
+	}
+	utils.LogVerbosef("Fetching all projects from %s GitLab instance", sourceString)
 	projects, _, err := g.Gitlab.Projects.ListProjects(nil)
 	if err != nil {
 		return err
 	}
 
+	utils.LogVerbosef("Processing %d projects from %s GitLab instance", len(projects), sourceString)
+
+	// Create a wait group to wait for all goroutines to finish
+	var wg sync.WaitGroup
+
+	// Create a channel to limit the number of concurrent goroutines
+	concurrencyLimit := 10
+	sem := make(chan struct{}, concurrencyLimit)
+
 	for _, project := range projects {
-		// Check if the project matches the filters:
-		//   - either is in the projects map
-		//   - or path starts with any of the groups in the groups map
-		if _, ok := filters.Projects[project.PathWithNamespace]; ok {
-			g.addProject(project.PathWithNamespace, project)
-		} else {
-			for group := range filters.Groups {
-				if strings.HasPrefix(project.PathWithNamespace, group) {
-					g.addProject(project.PathWithNamespace, project)
-					break
+		wg.Add(1)
+		// Acquire a token from the semaphore
+		sem <- struct{}{}
+
+		go func(project *gitlab.Project) {
+			defer wg.Done()
+			// Release the token back to the semaphore
+			defer func() { <-sem }()
+
+			// Check if the project matches the filters:
+			//   - either is in the projects map
+			//   - or path starts with any of the groups in the groups map
+			if _, ok := (*projectFilters)[project.PathWithNamespace]; ok {
+				g.addProject(project.PathWithNamespace, project)
+			} else {
+				for group := range *groupFilters {
+					if strings.HasPrefix(project.PathWithNamespace, group) {
+						// Add the project to the gitlab instance projects cache
+						g.addProject(project.PathWithNamespace, project)
+
+						if isSource {
+							// Retrieve the corresponding group creation options from the mirror mapping
+							groupCreationOptions, ok := mirrorMapping.Groups[group]
+							if !ok {
+								log.Fatalf("Group %s not found in mirror mapping (internal error, please review script)", group)
+							}
+
+							// Calculate the relative path between the project and the group
+							relativePath, err := filepath.Rel(group, project.PathWithNamespace)
+							if err != nil {
+								log.Fatalf("Failed to calculate relative path for project %s: %s", project.PathWithNamespace, err)
+							}
+
+							// Add the project to the mirror mapping
+							mirrorMapping.AddProject(project.PathWithNamespace, &utils.ProjectMirroringOptions{
+								CI_CD_Catalog:  groupCreationOptions.CI_CD_Catalog,
+								Issues:         groupCreationOptions.Issues,
+								DestinationURL: filepath.Join(groupCreationOptions.DestinationURL, relativePath),
+							})
+						}
+						break
+					}
 				}
 			}
-		}
+		}(project)
 	}
 
-	utils.LogVerbosef("Found %d projects to mirror in the source GitLab instance", len(g.Projects))
+	wg.Wait()
+
+	utils.LogVerbosef("Found %d projects to mirror in the %s GitLab instance", len(g.Projects), sourceString)
 
 	return nil
 }
 
-func (g *GitlabInstance) fetchGroups(filters *utils.MirrorMapping) error {
+func (g *GitlabInstance) fetchGroups(groupFilters *map[string]bool, mirrorMapping *utils.MirrorMapping, isSource bool) error {
+	sourceString := "source"
+	if !isSource {
+		sourceString = "destination"
+	}
+	utils.LogVerbosef("Fetching all groups from %s GitLab instance", sourceString)
 	groups, _, err := g.Gitlab.Groups.ListGroups(nil)
 	if err != nil {
 		return err
 	}
 
+	utils.LogVerbosef("Processing %d groups from %s GitLab instance", len(groups), sourceString)
+
+	// Create a wait group to wait for all goroutines to finish
+	var wg sync.WaitGroup
+
+	// Create a channel to limit the number of concurrent goroutines
+	concurrencyLimit := 10
+	sem := make(chan struct{}, concurrencyLimit)
+
 	for _, group := range groups {
-		// Check if the group matches the filters:
-		//   - either is in the groups map
-		//   - or path starts with any of the groups in the groups map
-		//   - or is a subgroup of any of the groups in the groups map
-		if _, ok := filters.Groups[group.FullPath]; ok {
-			g.addGroup(group.FullPath, group)
-		} else {
-			for groupPath := range filters.Groups {
-				if strings.HasPrefix(group.FullPath, groupPath) {
-					g.addGroup(group.FullPath, group)
-					break
+		wg.Add(1)
+		// Acquire a token from the semaphore
+		sem <- struct{}{}
+
+		go func(group *gitlab.Group) {
+			defer wg.Done()
+			// Release the token back to the semaphore
+			defer func() { <-sem }()
+
+			// Check if the group matches the filters:
+			//   - either is in the groups map
+			//   - or path starts with any of the groups in the groups map
+			//   - or is a subgroup of any of the groups in the groups map
+			if _, ok := (*groupFilters)[group.FullPath]; ok {
+				g.addGroup(group.FullPath, group)
+			} else {
+				for groupPath := range *groupFilters {
+					if strings.HasPrefix(group.FullPath, groupPath) {
+						// Add the group to the gitlab instance groups cache
+						g.addGroup(group.FullPath, group)
+
+						if isSource {
+							// Retrieve the corresponding group creation options from the mirror mapping
+							groupCreationOptions, ok := mirrorMapping.Groups[groupPath]
+							if !ok {
+								log.Fatalf("Group %s not found in mirror mapping (internal error, please review script)", groupPath)
+							}
+
+							// Calculate the relative path between the group and the groupPath
+							relativePath, err := filepath.Rel(groupPath, group.FullPath)
+							if err != nil {
+								log.Fatalf("Failed to calculate relative path for group %s: %s", group.FullPath, err)
+							}
+
+							// Add the group to the mirror mapping
+							mirrorMapping.AddGroup(group.FullPath, &utils.GroupMirroringOptions{
+								CI_CD_Catalog:  groupCreationOptions.CI_CD_Catalog,
+								Issues:         groupCreationOptions.Issues,
+								DestinationURL: filepath.Join(groupCreationOptions.DestinationURL, relativePath),
+							})
+						}
+						break
+					}
 				}
 			}
-		}
+		}(group)
 	}
 
-	utils.LogVerbosef("Found %d groups to mirror in the source GitLab instance", len(g.Groups))
+	wg.Wait()
+
+	utils.LogVerbosef("Found %d matching groups in %s GitLab instance", len(g.Groups), sourceString)
 
 	return nil
 }
 
-func (g *GitlabInstance) getParentGroupID(projectOrGroupPath string) (int, error) {
+func fetchAll(gitlabInstance *GitlabInstance, projectFilters map[string]bool, groupFilters map[string]bool, mirrorMapping *utils.MirrorMapping, isSource bool) error {
+	wg := sync.WaitGroup{}
+	errCh := make(chan error, 2)
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		if err := gitlabInstance.fetchGroups(&groupFilters, mirrorMapping, isSource); err != nil {
+			errCh <- err
+		}
+	}()
+	go func() {
+		defer wg.Done()
+		if err := gitlabInstance.fetchProjects(&projectFilters, &groupFilters, mirrorMapping, isSource); err != nil {
+			errCh <- err
+		}
+	}()
+	wg.Wait()
+	close(errCh)
+
+	return utils.MergeErrors(errCh, 2)
+}
+
+func (g *GitlabInstance) getParentNamespaceID(projectOrGroupPath string) (int, error) {
 	parentGroupID := -1
-	parentPath := path.Dir(projectOrGroupPath)
+	parentPath := filepath.Dir(projectOrGroupPath)
 	var err error = nil
-	if parentPath != "." {
+	if parentPath != "." && parentPath != "/" {
 		// Check if parent path is already in the instance groups cache
 		if parentGroup, ok := g.Groups[parentPath]; ok {
 			parentGroupID = parentGroup.ID
--- mirroring/post.go
+++ mirroring/post.go
@@ -1,10 +1,148 @@
 package mirroring
 
 import (
+	"fmt"
+	"log"
+	"sort"
+	"sync"
+
+	"github.com/boxboxjason/gitlab-sync/utils"
 	gitlab "gitlab.com/gitlab-org/api/client-go"
 )
 
-func (g *GitlabInstance) createProjectFromSource(sourceProject *gitlab.Project, destinationPath string) error {
+func createGroups(sourceGitlab *GitlabInstance, destinationGitlab *GitlabInstance, mirrorMapping *utils.MirrorMapping) error {
+	utils.LogVerbose("Creating groups in destination GitLab")
+	// Reverse the mirror mapping to get the source group path for each destination group
+	reversedMirrorMap := make(map[string]string, len(mirrorMapping.Groups))
+	// Extract the keys (group paths) and sort them
+	// This ensures that the parent groups are created before their children
+	destinationGroupPaths := make([]string, 0, len(sourceGitlab.Groups))
+	for sourceGroupPath, createOptions := range mirrorMapping.Groups {
+		reversedMirrorMap[createOptions.DestinationURL] = sourceGroupPath
+		destinationGroupPaths = append(destinationGroupPaths, createOptions.DestinationURL)
+	}
+	sort.Strings(destinationGroupPaths)
+
+	errorChan := make(chan error, len(destinationGroupPaths))
+	// Iterate over the groups in alphabetical order
+	for _, destinationGroupPath := range destinationGroupPaths {
+		// Retrieve the corresponding source group path
+		sourceGroupPath := reversedMirrorMap[destinationGroupPath]
+		utils.LogVerbosef("Mirroring group from source %s to destination %s", sourceGroupPath, destinationGroupPath)
+		sourceGroup := sourceGitlab.Groups[sourceGroupPath]
+		if sourceGroup == nil {
+			errorChan <- fmt.Errorf("Group %s not found in destination GitLab instance (internal error, please review script)", sourceGroupPath)
+			continue
+		}
+
+		// Retrieve the corresponding group creation options from the mirror mapping
+		groupCreationOptions, ok := mirrorMapping.Groups[sourceGroupPath]
+		if !ok {
+			errorChan <- fmt.Errorf("Source Group %s not found in mirror mapping (internal error, please review script)", sourceGroupPath)
+			continue
+		}
+
+		// Check if the group already exists in the destination GitLab instance
+		destinationGroup := destinationGitlab.getGroup(destinationGroupPath)
+		var err error
+		if destinationGroup != nil {
+			utils.LogVerbosef("Group %s already exists, skipping creation", destinationGroupPath)
+		} else {
+			utils.LogVerbosef("Creating group %s in destination GitLab instance", destinationGroupPath)
+			destinationGroup, err = destinationGitlab.createGroupFromSource(sourceGroup, groupCreationOptions)
+			if err != nil {
+				errorChan <- fmt.Errorf("Failed to create group %s in destination GitLab instance: %s", destinationGroupPath, err)
+				continue
+			} else {
+				err = sourceGitlab.copyGroupAvatar(destinationGitlab, destinationGroup, sourceGroup)
+				if err != nil {
+					errorChan <- fmt.Errorf("Failed to copy group avatar for %s: %s", destinationGroupPath, err)
+				}
+			}
+		}
+
+	}
+	close(errorChan)
+	return utils.MergeErrors(errorChan, 2)
+}
+
+func createProjects(sourceGitlab *GitlabInstance, destinationGitlab *GitlabInstance, mirrorMapping *utils.MirrorMapping) error {
+	utils.LogVerbose("Creating projects in destination GitLab instance")
+	// Reverse the mirror mapping to get the source project path for each destination project
+	reversedMirrorMap := make(map[string]string, len(mirrorMapping.Projects))
+	for sourceProjectPath, projectOptions := range mirrorMapping.Projects {
+		reversedMirrorMap[projectOptions.DestinationURL] = sourceProjectPath
+	}
+
+	// Create a wait group to wait for all goroutines to finish
+	var wg sync.WaitGroup
+
+	// Create a channel to collect errors
+	errorChan := make(chan error, len(reversedMirrorMap))
+
+	// Create a channel to limit the number of concurrent goroutines
+	concurrencyLimit := 10
+	sem := make(chan struct{}, concurrencyLimit)
+
+	for destinationProjectPath, sourceProjectPath := range reversedMirrorMap {
+		utils.LogVerbosef("Mirroring project from source %s to destination %s", sourceProjectPath, destinationProjectPath)
+		sourceProject := sourceGitlab.Projects[sourceProjectPath]
+		if sourceProject == nil {
+			errorChan <- fmt.Errorf("Project %s not found in source GitLab instance (internal error, please review script)", sourceProjectPath)
+			continue
+		}
+		wg.Add(1)
+		// Acquire a token from the semaphore
+		sem <- struct{}{}
+
+		go func(sourcePath string, destinationPath string) {
+			defer wg.Done()
+			// Release the token back to the semaphore
+			defer func() { <-sem }()
+
+			// Retrieve the corresponding project creation options from the mirror mapping
+			projectCreationOptions, ok := mirrorMapping.Projects[sourcePath]
+			if !ok {
+				errorChan <- fmt.Errorf("Project %s not found in mirror mapping (internal error, please review script)", sourcePath)
+				return
+			}
+
+			// Check if the project already exists
+			destinationProject := destinationGitlab.getProject(destinationPath)
+			var err error
+			if destinationProject != nil {
+				utils.LogVerbosef("Project %s already exists, skipping creation", destinationPath)
+			} else {
+				sourceProject := sourceGitlab.Projects[sourcePath]
+				if sourceProject == nil {
+					errorChan <- fmt.Errorf("Project %s not found in source GitLab instance (internal error, please review script)", sourcePath)
+					return
+				}
+				destinationProject, err = destinationGitlab.createProjectFromSource(sourceProject, projectCreationOptions)
+				if err != nil {
+					errorChan <- fmt.Errorf("Failed to create project %s in destination GitLab instance: %s", destinationPath, err)
+					return
+				}
+			}
+
+			err = destinationGitlab.updateProjectFromSource(sourceGitlab, sourceProject, destinationProject, projectCreationOptions)
+			if err != nil {
+				errorChan <- fmt.Errorf("Failed to update project %s in destination GitLab instance: %s", destinationPath, err)
+				return
+			}
+
+			log.Printf("Completed mirroring project to %s", destinationPath)
+		}(sourceProjectPath, destinationProjectPath)
+	}
+
+	// Wait for all goroutines to finish
+	wg.Wait()
+	close(errorChan)
+
+	return utils.MergeErrors(errorChan, 2)
+}
+
+func (g *GitlabInstance) createProjectFromSource(sourceProject *gitlab.Project, copyOptions *utils.ProjectMirroringOptions) (*gitlab.Project, error) {
 	projectCreationArgs := &gitlab.CreateProjectOptions{
 		Name:                &sourceProject.Name,
 		Path:                &sourceProject.Path,
@@ -13,21 +151,28 @@ func (g *GitlabInstance) createProjectFromSource(sourceProject *gitlab.Project, 
 		MirrorTriggerBuilds: gitlab.Ptr(true),
 		Mirror:              gitlab.Ptr(true),
 		Topics:              &sourceProject.Topics,
-		Visibility:          &sourceProject.Visibility,
 	}
 
+	utils.LogVerbosef("Retrieving project namespace ID for %s", copyOptions.DestinationURL)
+	parentNamespaceId, err := g.getParentNamespaceID(copyOptions.DestinationURL)
+	if err != nil {
+		return nil, err
+	} else if parentNamespaceId >= 0 {
+		projectCreationArgs.NamespaceID = &parentNamespaceId
+	}
+
+	utils.LogVerbosef("Creating project %s in destination GitLab instance", copyOptions.DestinationURL)
 	destinationProject, _, err := g.Gitlab.Projects.CreateProject(projectCreationArgs)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	g.addProject(destinationPath, destinationProject)
+	utils.LogVerbosef("Project %s created successfully", destinationProject.PathWithNamespace)
+	g.addProject(copyOptions.DestinationURL, destinationProject)
 
-	err = g.enableProjectMirrorPull(sourceProject, destinationProject)
-
-	return err
+	return destinationProject, nil
 }
 
-func (g *GitlabInstance) createGroupFromSource(sourceGroup *gitlab.Group, destinationPath string) error {
+func (g *GitlabInstance) createGroupFromSource(sourceGroup *gitlab.Group, copyOptions *utils.GroupMirroringOptions) (*gitlab.Group, error) {
 	groupCreationArgs := &gitlab.CreateGroupOptions{
 		Name:          &sourceGroup.Name,
 		Path:          &sourceGroup.Path,
@@ -36,16 +181,17 @@ func (g *GitlabInstance) createGroupFromSource(sourceGroup *gitlab.Group, destin
 		DefaultBranch: &sourceGroup.DefaultBranch,
 	}
 
-	parentGroupID, err := g.getParentGroupID(destinationPath)
+	parentGroupID, err := g.getParentNamespaceID(copyOptions.DestinationURL)
 	if err != nil {
-		return err
+		return nil, err
 	} else if parentGroupID >= 0 {
 		groupCreationArgs.ParentID = &parentGroupID
 	}
 
 	destinationGroup, _, err := g.Gitlab.Groups.CreateGroup(groupCreationArgs)
 	if err == nil {
-		g.addGroup(destinationPath, destinationGroup)
+		g.addGroup(copyOptions.DestinationURL, destinationGroup)
 	}
-	return err
+
+	return destinationGroup, err
 }
